423455b38b561675f0a5b49ad47f75c26aa08cd6
Viktor Ivannikov <iceivannikov@gmail.com>
2025-07-30 21:31:27 +0300

    1. Подключение к базе в веб приложении. Хранение вакансий. [#504859]

--- src/main/java/ru/job4j/dreamjob/model/City.java
+++ src/main/java/ru/job4j/dreamjob/model/City.java
@@ -1,4 +1,27 @@
 package ru.job4j.dreamjob.model;
 
-public record City(int id, String name) {
+public final class City {
+    private int id;
+    private String name;
+
+    public City(int id, String name) {
+        this.id = id;
+        this.name = name;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
 }
